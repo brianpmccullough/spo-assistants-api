18f274caf45dbf670b08d061e6536c007c66cf7a
Brian P. McCullough <brianpmccullough@gmail.com>
2026-07-18 11:42:30 -0400

    add logging on startup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  Logger.log(`Application is running on: ${await app.getUrl()}`, 'Bootstrap');
 }
 bootstrap();
